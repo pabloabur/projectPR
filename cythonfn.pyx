5828a77744a65cfc499a9557a61d0b91908c7f1a
Pablo Alejandro <pabloabur@gmail.com>
2018-05-11 22:59:35 -0300

    small tests on same code

--- cythonfn.pyx
+++ cythonfn.pyx
@@ -1,13 +1,18 @@
-#cython: boundscheck=False
 import numpy as np
 
+#pythran export runge_kutta(float, float64[], float, float, float, int)
 def runge_kutta(t, x, timeStep, timeStepByTwo, timeStepBySix, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv):
     k1 = dVdt(t, x, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k2 = dVdt(t + timeStepByTwo, x + timeStepByTwo * k1, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k3 = dVdt(t + timeStepByTwo, x + timeStepByTwo * k2, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k4 = dVdt(t + timeStep, x + timeStep * k3, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     
-    return x + timeStepBySix * (np.add(np.add(np.add(k1, k2, order = 'C'), np.add(k2, k3, order='C')), np.add(k3, k4, order='C'), order='C'))
+    return x + timeStepBySix * (np.add(
+                                       np.add(
+                                             np.add(k1, k2, order = 'C'), np.add(k2, k3, order='C')
+                                             ),
+                                       np.add(k3, k4, order='C'), order='C')
+                               )
 
 def dVdt(t, V, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv):
     #k = 0
--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy as np
+
 setup(
         cmdclass = {'build_ext': build_ext},
-        ext_modules = [Extension("calculate", ["cythonfn.pyx"])]
+        ext_modules = [Extension("calculate", ["cythonfn.pyx"],include_dirs=[np.get_include()])]
         )

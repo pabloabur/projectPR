e4d4a62b68e774278a0c1df60c1c73f153ccaa9e
Pablo Alejandro <pabloabur@gmail.com>
2018-05-14 19:20:19 -0300

    last try with current strategy

--- cythonfn.pyx
+++ cythonfn.pyx
@@ -1,26 +1,75 @@
-import numpy as np
+#cython: boundscheck=False
 
-#pythran export runge_kutta(float, float64[], float, float, float, int)
-def runge_kutta(t, x, timeStep, timeStepByTwo, timeStepBySix, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv):
+import numpy as np
+cimport numpy as np
+
+def runge_kutta(float t, double[:] x, float timeStep, float timeStepByTwo,
+        float timeStepBySix, unsigned int MUnumber, unit, double[:] iIonic,
+        G, double[:] iInjected,
+        double[:] EqCurrent_nA, double[:] capacitanceInv):
     k1 = dVdt(t, x, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k2 = dVdt(t + timeStepByTwo, x + timeStepByTwo * k1, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k3 = dVdt(t + timeStepByTwo, x + timeStepByTwo * k2, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     k4 = dVdt(t + timeStep, x + timeStep * k3, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv)
     
-    return x + timeStepBySix * (np.add(
-                                       np.add(
-                                             np.add(k1, k2, order = 'C'), np.add(k2, k3, order='C')
-                                             ),
-                                       np.add(k3, k4, order='C'), order='C')
-                               )
+    #xout = x + timeStepBySix * (np.add(
+    #                                   np.add(
+    #                                         np.add(k1, k2, order = 'C'), np.add(k2, k3, order='C')
+    #                                         ),
+    #                                   np.add(k3, k4, order='C'), order='C')
+    #                           )
+    xout = x + timeStepBySix * k1 + k2 + k2 + k3 + k3 + k4
+    return xout
 
-def dVdt(t, V, MUnumber, unit, iIonic, G, iInjected, EqCurrent_nA, capacitanceInv):
+'''np.ndarray[np.float, ndim=2] G'''
+
+def dVdt(float t, double[:] V, unsigned int MUnumber, unit, double[:] iIonic,
+        G, double[:] iInjected,
+        double[:] EqCurrent_nA, double[:] capacitanceInv):
     #k = 0
+    cdef unsigned int i, j
+    cdef int[:] compNumber = np.empty(len(unit), dtype=np.int32)
+
+    for i in xrange(len(unit)):
+        compNumber[i] = unit[i].compNumber
+
     for i in xrange(MUnumber):
-        for j in xrange(unit[i].compNumber):
-            iIonic.itemset(i*unit[0].compNumber+j,
-                                unit[i].compartment[j].computeCurrent(t,
-                                                                        V.item(i*unit[0].compNumber+j)))
+        for j in xrange(compNumber[i]):
+            iIonic[i*compNumber[0]+j] = computeCurrent(t,
+                                        V[i*compNumber[0]+j],
+                                        unit[i].compartment[j])
             #k += 1
           
-    return (iIonic + G.dot(V) + iInjected + EqCurrent_nA) * capacitanceInv
+    res = G.dot(V)
+    Vout = (iIonic + res + iInjected + EqCurrent_nA) * capacitanceInv
+    return Vout
+
+def computeCurrent(self, t, V_mV, compartment):
+    I = 0.0
+
+    if comparment.SynapsesIn[0].numberOfIncomingSynapses:
+        I += computeSynapticCurrent(t, V_mV, comparment.SynapsesIn[0])
+    if comparment.SynapsesIn[1].numberOfIncomingSynapses:
+        I += computeSynapticCurrent(t, V_mV, comparment.SynapsesIn[1])
+    for i in xrange(0, self.numberChannels):
+        I += self.Channels[i].computeChannelCurrent(t, V_mV)
+    
+    return I
+
+def computeSynapticCurrent(self, t, V_mV, synapse):
+    if len(self.tEndOfPulse) == 0:
+        self.tBeginOfPulse = np.ones_like(self.gmax_muS,
+                                          dtype=float) * float("-inf")
+        self.tEndOfPulse = np.ones_like(self.gmax_muS,
+                                        dtype=float) * float("-inf")
+        self.tLastPulse = np.ones_like(self.gmax_muS,
+                                       dtype=float) * float("-inf")
+        self.conductanceState = np.zeros_like(self.gmax_muS,
+                                              dtype=int)
+        self.ri = np.zeros_like(self.gmax_muS, dtype=float)
+        self.ti = np.zeros_like(self.gmax_muS, dtype=float)
+        self.dynamicGmax = np.zeros_like(self.gmax_muS, dtype=float)
+        self.synContrib = self.gmax_muS / self.gMaxTot_muS
+        self.computeCurrent = self.computeCurrent2
+    
+    return self.computeConductance(t) * (self.EqPot_mV - V_mV)
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+#from distutils.extension import Extension
+#from Cython.Distutils import build_ext
 
 import numpy as np
 
-setup(
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = [Extension("calculate", ["cythonfn.pyx"],include_dirs=[np.get_include()])]
-        )
+ext = Extension(name="calculate",
+                sources=["cythonfn.pyx"],
+                include_dirs=[np.get_include()])
+setup( #cmdclass = {'build_ext': build_ext},
+      ext_modules = cythonize(ext)#[Extension("calculate", ["cythonfn.pyx"],include_dirs=[np.get_include()])]
+      )
